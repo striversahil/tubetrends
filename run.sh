e9d20702278518bc32ee556ea30ddc7eff1327f4
Sahil Kumar Jha <striversahil@gmail.com>
2025-05-15 07:12:12 +0530

    Initial project setup with basic Python files and development environment

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+# Making an venv Envrionment
+
+python3 -m venv venv
+
+# Activating the venv
+source venv/bin/activate
---
+++ src/main.py
@@ -0,0 +1,8 @@
+if (__name__ == "__main__"):
+      print("Hello World")
+      print("Hello Again")
+      print("I like typing this.")
+      print("This is fun.")
+      print("Yay! Printing.")
+      print("I'd much rather you 'not'.")
+      print('I "said" do not touch this.')
