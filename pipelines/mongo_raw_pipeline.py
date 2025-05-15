0b62971e382d0e1392b5699db0dbfd246cf04937
Sahil Kumar Jha <striversahil@gmail.com>
2025-05-15 20:10:07 +0530

    Update SQL params and refactor pipeline code for better readability

--- pipelines/mongo_raw_pipeline.py
+++ pipelines/mongo_raw_pipeline.py
@@ -1,18 +1,16 @@
-
 from connection.mongoDb import Db
 from datetime import datetime
 
+
 # Storing the trending data with timestamp
-def StoreTrending(data : dict):
-      trendingData = {
-          "timestamp" : datetime.now(),
-          "trending_data" : data
-      }
-      try:
-            Db["Trending"].insert_one(trendingData)
-            return True
-      except :
-            return False
-      
+def StoreTrending(data: dict):
+    trendingData = {"timestamp": datetime.now(), "trending_data": data}
+    try:
+        Db["Trending"].insert_one(trendingData)
+        return True
+    except:
+        return False
+
+
 # data = getTrending(regionCode)
 # StoreTrending(data)
--- pipelines/storing_rawTransformed.py
+++ pipelines/storing_rawTransformed.py
@@ -2,45 +2,43 @@
 from os import path
 from connection.postgres import cur
 
+
 def CreateTableandInsert() -> dict | bool:
     try:
-        pat = path.dirname(path.realpath(__file__))
-        pth = pat.split("/")[:-1]
-        ph = "/".join(pth)
 
+        base_path = "/".join(path.dirname(path.realpath(__file__)).split("/")[:-1])
         video = None
         trending = None
 
-        with open(ph + "/sql/createRawTransformed_trending.sql", "r") as f:
+        with open(base_path + "/sql/createRawTransformed_trending.sql", "r") as f:
             cur.execute(f.read())
 
-        with open(ph + "/sql/createRawTransformed_video.sql", "r") as f:
+        with open(base_path + "/sql/createRawTransformed_video.sql", "r") as f:
             cur.execute(f.read())
 
-        with open(ph + "/sql/insertRawTransformed_trending.sql", "r") as f:
+        with open(base_path + "/sql/insertRawTransformed_trending.sql", "r") as f:
             trending = f.read()
 
-        with open(ph + "/sql/insertRawTransformed_video.sql", "r") as f:
+        with open(base_path + "/sql/insertRawTransformed_video.sql", "r") as f:
             video = f.read()
-        
-        return { trending, video }
-        
+
+        return {trending, video}
+
     except Exception as e:
         print("Something went wrong 😬: \n", e)
         return False
-    
 
 
-
-def StoreRawTransformed(data : dict):
+def StoreRawTransformed(data: dict):
     try:
         res = CreateTableandInsert()
-        if res is not False: 
+        if res is not False:
             cur.execute(res[0], [data])
             cur.execute(res[1], [data])
-        
+
     except Exception as e:
         print("Something went wrong 😬: \n", e)
+
 
 # cur.execute("SELECT NOW()")
 # print(cur.fetchone())
