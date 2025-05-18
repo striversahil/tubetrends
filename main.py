b5cd7f0730d69b9cb675665428e97546d1de709b
Sahil Kumar Jha <striversahil@gmail.com>
2025-05-18 15:13:13 +0530

    Add git pull before commit and archive old workflow file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pipelines.o1storing_rawVideo import storeRawVideo
 from pipelines.o2storing_rawChannel import storeRawChannel
-from connection.postgres import PostgresConnection
 from dotenv import load_dotenv
 
 
@@ -11,8 +10,9 @@ if __name__ == "__main__":
     #                      +++++++++++++++++++++++++++++ Pipeline Started ++++++++++++++++++++++++++++
     # Storing Raw Data in Mongo DB in fixed interval
     channels = storeRawVideo()
-    if channels is False:
+    if not channels:
         print("❌ Error in storing raw video Pipeline")
+        exit(1)
     else:
         print("✅ Raw Video Pipeline Completed")
         storeRawChannel(channels)
