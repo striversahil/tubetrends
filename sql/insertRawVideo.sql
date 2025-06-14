4b007522c2a2c350974025be91449cd155b3a01f
Sahil Kumar Jha <striversahil@gmail.com>
2025-06-14 17:44:31 +0530

    changed rankscore

--- sql/insertRawVideo.sql
+++ sql/insertRawVideo.sql
@@ -1,4 +1,4 @@
-INSERT INTO video (region , ranking, videoId, title, trending_id, publishedAt,category, channelId, channelName, thumbnail, tags, duration, viewCount, likeCount, commentCount) VALUES (
+INSERT INTO video (region , rankScore, videoId, title, trending_id, publishedAt,category, channelId, channelName, thumbnail, tags, duration, viewCount, likeCount, commentCount) VALUES (
     %s,
     %s,
     %s,
